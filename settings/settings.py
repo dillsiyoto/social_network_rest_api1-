8041ce974ac99564b5ff93967c501ee67544b96a
Adilya Amanbay <adilya.amanbay@gmail.com>
2025-09-23 21:26:47 +0500

    configure postgres db

--- settings/settings.py
+++ settings/settings.py
@@ -75,8 +75,12 @@ CORS_ALLOW_ORIGINS = []
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASS"),
+        "HOST": config("DB_HOST"),
+        "PORT": config("DB_PORT"),
     }
 }
 
